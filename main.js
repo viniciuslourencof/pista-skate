9060f2b46eb39a65a2b2f6c782e354c00a8f246d
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2022-12-03 11:07:12 -0300

    Criação do layout desktop

--- main.js
+++
@@ -1,6 +0,0 @@
-const botao_menu = document.querySelector('.cabecalho__menu');
-const menu_lateral = document.querySelector('.menu-lateral');
-
-botao_menu.onclick = function () {
-    menu_lateral.classList.toggle('menu-lateral--invisivel')
-}
